2a0b839ec0ecc5ebb08250a856a9b3cdfac94c4c
britishandbohemian <48567677+britishandbohemian@users.noreply.github.com>
2024-08-20 02:07:08 +0200

    New

--- src/client/index.js
+++ src/client/index.js
@@ -1,74 +1,31 @@
-/*!
- * OS.js - JavaScript Cloud/Web Desktop Platform
- *
- * Copyright (c) 2011-2020, Anders Evenrud <andersevenrud@gmail.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * @author  Anders Evenrud <andersevenrud@gmail.com>
- * @licence Simplified BSD License
- */
-
-//
-// This is the client bootstrapping script.
-// This is where you can register service providers or set up
-// your libraries etc.
-//
-// https://manual.os-js.org/guide/provider/
-// https://manual.os-js.org/install/
-// https://manual.os-js.org/resource/official/
-//
-
-import {
+const {
   Core,
   CoreServiceProvider,
-  DesktopServiceProvider,
+  PackageServiceProvider,
   VFSServiceProvider,
-  NotificationServiceProvider,
-  SettingsServiceProvider,
-  AuthServiceProvider
-} from '@osjs/client';
+  AuthServiceProvider,
+  SettingsServiceProvider
+} = require('@osjs/server');
 
-import {PanelServiceProvider} from '@osjs/panels';
-import {GUIServiceProvider} from '@osjs/gui';
-import {DialogServiceProvider} from '@osjs/dialogs';
-import config from './config.js';
-import './index.scss';
+const config = require('./config.js');
+const osjs = new Core(config, {});
 
-const init = () => {
-  const osjs = new Core(config, {});
+osjs.register(CoreServiceProvider, {before: true});
+osjs.register(PackageServiceProvider);
+osjs.register(VFSServiceProvider);
+osjs.register(AuthServiceProvider); // This is important for handling /login
+osjs.register(SettingsServiceProvider);
 
-  // Register your service providers
-  osjs.register(CoreServiceProvider);
-  osjs.register(DesktopServiceProvider);
-  osjs.register(VFSServiceProvider);
-  osjs.register(NotificationServiceProvider);
-  osjs.register(SettingsServiceProvider, {before: true});
-  osjs.register(AuthServiceProvider, {before: true});
-  osjs.register(PanelServiceProvider);
-  osjs.register(DialogServiceProvider);
-  osjs.register(GUIServiceProvider);
+const shutdown = signal => (error) => {
+  if (error instanceof Error) {
+    console.error(error);
+  }
 
-  osjs.boot();
+  osjs.destroy(() => process.exit(signal));
 };
 
-window.addEventListener('DOMContentLoaded', () => init());
+process.on('SIGTERM', shutdown(0));
+process.on('SIGINT', shutdown(0));
+process.on('exit', shutdown(0));
+
+osjs.boot().catch(shutdown(1));
